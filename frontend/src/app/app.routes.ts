bac6a9990336ef2aa842c8b1d4719b633975db0f
Edwin Gutar <edwin.gutar@sonem.org>
2025-05-13 10:49:48 +0200

    routing implementiert

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { WindowTitleComponent } from './components/window-title/window-title.component';
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { ConfirmButtonComponent } from './components/confirm-button/confirm-button.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthViewComponent } from './components/auth-view/auth-view.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'windowTitle', component: WindowTitleComponent},
+  { path: 'InputField', component: InputFieldComponent },
+  { path: 'ConfirmButton', component: ConfirmButtonComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'AuthView', component: AuthViewComponent },
+
+];
