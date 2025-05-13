cdd156fd17ba0ff8d38dcd290e7cbc1cde520a9c
Edwin Gutar <edwin.gutar@sonem.org>
2025-05-13 09:55:45 +0200

    InputField dient als Eingabefeld

---
+++ frontend/src/app/components/input-field/input-field.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-input-field',
+  standalone: true,
+  templateUrl: './input-field.component.html',
+  styleUrls: ['./input-field.component.css'],
+  imports: [NgStyle]
+})
+export class InputFieldComponent {
+  private _fieldValue: string = '';
+  @Output() fieldValueChange = new EventEmitter<string>();
+
+  @Input() placeholder = 'E-Mail eingeben';
+  @Input() backgroundColor = '#E6E7E9';
+  @Input() type: 'text' | 'password' = 'text';
+
+  @Input() height: string = '50px';
+  @Input() minWidth: string = '325px';
+  @Input() maxWidth: string = '500px';
+
+  @Input()
+  set fieldValue(val: string) {
+    if (val !== this._fieldValue) {
+      this._fieldValue = val;
+    }
+  }
+  get fieldValue(): string {
+    return this._fieldValue;
+  }
+
+  onUserInput(val: string) {
+    this._fieldValue = val;
+    this.fieldValueChange.emit(val);
+  }
+}
