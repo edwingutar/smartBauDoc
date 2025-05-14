d1ac3da38d97dee6264095e018c8094955e9b581
Edwin Gutar <edwin.gutar@sonem.org>
2025-05-14 12:49:50 +0200

    Service und Model mit eingebunden + HTML (Banana in a Box)

--- frontend/src/app/components/add-project/add-project.component.ts
+++ frontend/src/app/components/add-project/add-project.component.ts
@@ -2,6 +2,8 @@ import {Component, Input} from '@angular/core';
 import {ConfirmButtonComponent} from '../confirm-button/confirm-button.component';
 import {InputFieldComponent} from '../input-field/input-field.component';
 import {WindowTitleComponent} from '../window-title/window-title.component';
+import {Project} from '../../models/project.model';
+import {ProjectService} from '../../services/project.service';
 
 @Component({
   selector: 'app-add-project',
@@ -30,5 +32,50 @@ export class AddProjectComponent {
   @Input() fourthPlaceholder = 'Details';
   @Input() fifthPlaceholder = 'Ansprechpartner';
   @Input() sixthPlaceholder = 'Deadline';
+
+  project: Project = {
+    title: '',
+    task: '',
+    pn: '',
+    address: '',
+    details: '',
+    contact: '',
+    deadline: ''
+  };
+
+  constructor(private projectService: ProjectService) {}
+
+  saveProject() {
+    this.projectService.createProject(this.project).subscribe({
+      next: (res) => {
+        console.log('Projekt erfolgreich gespeichert:', res);
+      },
+      error: (err) => {
+        console.error('Fehler beim Speichern:', err);
+      }
+    });
+  }
+
+  // Test
+  test():void {
+    console.log(this.project.task)
+  }
+  test2():void {
+    console.log(this.project.pn)
+  }
+  test3():void {
+    console.log(this.project.address)
+  }
+  test4():void {
+    console.log(this.project.details)
+  }
+  test5():void {
+    console.log(this.project.contact)
+  }
+  test6():void {
+    console.log(this.project.deadline)
+  }
+
+
 
 }
