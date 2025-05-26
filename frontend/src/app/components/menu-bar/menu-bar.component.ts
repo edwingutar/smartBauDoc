a0956b836ab3542729272d9eba29b7808c51b569
Edwin Gutar <edwin.gutar@sonem.org>
2025-05-26 15:19:22 +0200

    Menubar zur Navigation

---
+++ frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu-bar',
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './menu-bar.component.html',
+  standalone: true,
+  styleUrl: './menu-bar.component.css'
+})
+export class MenuBarComponent {
+
+}
