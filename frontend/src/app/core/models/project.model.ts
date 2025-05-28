d8f8651ce9155957f9d04da716d1d4bafe913760
Edwin Gutar <edwin.gutar@sonem.org>
2025-05-28 17:47:34 +0200

    Project Model Variablen angepasst ans Backend (Entries + Ansprechpartner)

--- frontend/src/app/core/models/project.model.ts
+++ frontend/src/app/core/models/project.model.ts
@@ -1,8 +1,12 @@
+import {Entry} from './entry.model';
+
 export interface Project {
   title: string;
   detail: string;
+  ansprechpartner: string,
   street: string;
   pCode: string;
   sDate: string;
   dDate: string;
+  entries: Entry[];
 }
--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Project } from '../models/project.model';
+import { Project } from '../core/models/project.model';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
