2c3858b98c3c8e3273498d65beb14db9f998ce58
Lukas Bulun <lukas.bulun@hs-augsburg.de>
2025-05-28 23:06:51 +0200

    LoginKomp - Frontend Logik + LoginService implementiert

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
+.TS:
 import { Component } from '@angular/core';
 import { InputFieldComponent } from '../input-field/input-field.component';
 import { ConfirmButtonComponent } from '../confirm-button/confirm-button.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +11,44 @@ import { ConfirmButtonComponent } from '../confirm-button/confirm-button.compone
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
   email = '';
   password = '';
 
+
+
+ constructor(private authService: AuthService) {}
+
+   saveClick: () => void = () => {
+    this.onLogin();
+  };
+
+//On login Methode - wird aufgerufen, wenn der Benutzer auf den Login-Button klickt
+ onLogin() {
+      console.log('Login:', {
+      email: this.email,
+      password: this.password,
+    });
+    // Validierung der Eingaben 
+    if (!this.email || !this.password) {
+      alert('Bitte geben Sie Ihre E-Mail und Ihr Passwort ein.')
+      return;
+    }else{
+
+    this.authService.login(this.email, this.password)
+    .subscribe({
+      next: (res) => {
+        // Weiterleitung oder Status speichern
+        alert('Erfolgreich eingeloggt!');
+        console.log('Login erfolgreich:', res);
+      },
+      error: (err) => {
+        alert('Login fehlgeschlagen! Bitte überprüfen Sie Ihre Anmeldedaten.');
+        console.error('Login Fehler:', err);
+      }
+    });
+  }
+
+}
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -4,10 +4,19 @@ import { Observable } from 'rxjs';
 
 
 @Injectable({ providedIn: 'root' })
+
 export class AuthService {
   constructor(private http: HttpClient) {}
 
+//registerservice
   register(user: { email: string; name: string; password: string }): Observable<any> {
     return this.http.post('http://localhost:8080/api/register', user);
   }
+
+
+//loginservice
+  login(email: string, password: string): Observable<any> {
+    return this.http.post('http://localhost:8080/api/login', { username: email, password });
+  }
+
 }
