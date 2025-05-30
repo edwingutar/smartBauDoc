da9fa087e620abd0ed85f8dd1f9de6b999114e19
Edwin Gutar <edwin.gutar@sonem.org>
2025-05-30 08:14:03 +0200

    MenuBar dynamisch angepasst an andere Fenster + Icon eingebaut

--- frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-menu-bar',
   imports: [
-    RouterModule
+    RouterModule,
+    NgStyle
   ],
   templateUrl: './menu-bar.component.html',
   standalone: true,
   styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent {
+
+  @Input() width: string = '90vw';
+  @Input() minWidth: string = '350px';
+  @Input() maxWidth: string = '1200px';
+
+  @Input() height: string = '75px';
+
 
 }
