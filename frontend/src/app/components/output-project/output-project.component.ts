fc0cf55e6c0ed23c9caa7bf3b6463c3f8fbe852c
Edwin Gutar <edwin.gutar@sonem.org>
2025-05-30 08:59:33 +0200

    Project Card dynamisch angepasst + Design optimiert

--- frontend/src/app/components/output-project/output-project.component.ts
+++ frontend/src/app/components/output-project/output-project.component.ts
@@ -1,16 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../../core/models/project.model';
+import {NgStyle} from '@angular/common';
 
 
 @Component({
   selector: 'app-output-project',
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './output-project.component.html',
   standalone: true,
   styleUrl: './output-project.component.css'
 })
 export class OutputProjectComponent {
   @Input() project!: Project;
+
+  @Input() width: string = '85vw';
+  @Input() minWidth: string = '300px';
+  @Input() maxWidth: string = '1000px';
+
+  @Input() height: string = '110px';
 
   showInfo(): void {
     console.log("Ich bin aktiv!", this.project);
