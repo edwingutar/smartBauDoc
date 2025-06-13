399f3b08438a8df74b3d7cecb22a140235d92864
Edwin Gutar <edwin.gutar@sonem.org>
2025-06-13 13:12:24 +0200

    showInfo soll weiter leiten zu Projekt Einträgen, gibt ProjektName für Überschrift mit sowie ID um einträge im Projekt anzulegen

--- frontend/src/app/components/output-project/output-project.component.ts
+++ frontend/src/app/components/output-project/output-project.component.ts
@@ -1,15 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../../core/models/project.model';
-import {NgStyle} from '@angular/common';
-
+import { Router } from '@angular/router';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-output-project',
-  imports: [
-    NgStyle
-  ],
-  templateUrl: './output-project.component.html',
   standalone: true,
+  imports: [NgStyle],
+  templateUrl: './output-project.component.html',
   styleUrl: './output-project.component.css'
 })
 export class OutputProjectComponent {
@@ -18,10 +16,20 @@ export class OutputProjectComponent {
   @Input() width: string = '85vw';
   @Input() minWidth: string = '300px';
   @Input() maxWidth: string = '1000px';
-
   @Input() height: string = '110px';
 
+  constructor(private router: Router) {}
+
   showInfo(): void {
-    console.log("Ich bin aktiv!", this.project);
+    if (this.project?.id) {
+      this.router.navigate(['/menuBar/ProjectEntries'], {
+        state: {
+          projectId: this.project.id,
+          projectName: this.project.title}
+      });
+      console.log("Projekt titel", this.project.title)
+    } else {
+      console.warn('Keine gültige Projekt-ID vorhanden');
+    }
   }
 }
