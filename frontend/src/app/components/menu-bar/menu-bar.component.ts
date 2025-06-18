15d0b9afeb5a646685437bb7ad26834c08827367
Lukas Lehrer <lukas.lehrer@hs-augsburg.de>
2025-06-19 01:46:37 +0200

    menuBar -> Knöpfe angepasst und Logout hinzugefügt

--- frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {NgStyle} from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu-bar',
@@ -17,8 +18,12 @@ export class MenuBarComponent {
   @Input() width: string = '90vw';
   @Input() minWidth: string = '300px';
   @Input() maxWidth: string = '1180px';
-
   @Input() height: string = '75px';
 
+  constructor(private authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
